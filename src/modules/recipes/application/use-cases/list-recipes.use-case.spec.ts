e4ec8df79e7f5d06cc7be49b111e58ad78b68cb4
Edilaine Santos <edilaine.santos839@gmail.com>
2026-01-07 00:01:58 -0300

    test: improve coverage for listing user recipes

--- src/modules/recipes/application/use-cases/list-recipes.use-case.spec.ts
+++ src/modules/recipes/application/use-cases/list-recipes.use-case.spec.ts
@@ -23,19 +23,48 @@ describe('ListRecipeUseCase', () => {
 
     expect(resultEmpty).toEqual([])
 
-    const recipe = Recipe.create({
+    const recipe1 = Recipe.create({
       title: 'Pancake',
       category: RecipeCategory.DESSERT,
       userId,
     })
 
-    await recipeRepository.save(recipe)
+    const recipe2 = Recipe.create({
+      title: 'Omelette',
+      category:
+        RecipeCategory.MAIN_DISH,
+      userId,
+    })
+
+    const otherUserRecipe =
+      Recipe.create({
+        title: 'Pizza',
+        category:
+          RecipeCategory.MAIN_DISH,
+        userId: 'other-user',
+      })
+
+    await recipeRepository.save(recipe1)
+    await recipeRepository.save(recipe2)
+    await recipeRepository.save(
+      otherUserRecipe,
+    )
 
     const recipeReturnUseCase =
       await useCase.execute(userId)
 
     expect(
-      recipeReturnUseCase![0].getTitle(),
-    ).toBe('Pancake')
+      recipeReturnUseCase,
+    ).toHaveLength(2)
+    expect(
+      recipeReturnUseCase!.map((r) =>
+        r.getTitle(),
+      ),
+    ).toEqual(
+      expect.arrayContaining([
+        'Pancake',
+        'Omelette',
+      ]),
+    )
   })
 })
